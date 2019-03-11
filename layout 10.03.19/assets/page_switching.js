f09c7fa0a9016315ab18cb722d1c66ac677548c7
Volodymyr_Derunov <vderunov@gmail.com>
2019-03-11 20:14:53 +0200

    Create HW_21

--- layout 10.03.19/assets/page_switching.js
+++ layout 10.03.19/assets/page_switching.js
@@ -1,61 +1,46 @@
 let link_buttons_ = document.getElementById('link_buttons');
-let section_home_ = document.getElementById('section_home');
-let link_home_ = document.getElementById('link_home');
+let section_slider_ = document.getElementById('section_slider');
+let link_slider_ = document.getElementById('link_slider');
 let section_buttons_ = document.getElementById('section_buttons');
 let link_table_ = document.getElementById('link_table');
 let section_table_ = document.getElementById('section_table');
-let link_slider_ = document.getElementById('link_slider');
-let section_slider_ = document.getElementById('section_slider');
 let link_stud_table_ = document.getElementById('link_stud_table');
 let section_stud_table_ = document.getElementById('section_stud_table');
 
-link_home_.addEventListener('click', changeSectionHome);
+link_slider_.addEventListener('click', changeSectionSlider);
 link_buttons_.addEventListener('click', changeSectionButtons);
 link_table_.addEventListener('click', changeSectionTable);
-link_slider_.addEventListener('click', changeSectionSlider);
 link_stud_table_.addEventListener('click', changeSectionStudTable);
-
-function changeSectionHome() {
-    if (section_home_.className === 'display_none');
-    section_home_.className = 'display_block';
-    section_buttons_.className = 'display_none'
-    section_table_.className = 'display_none'
-    section_slider_.className = 'display_none'
-    section_stud_table_.className = 'display_none'
-}
-
-function changeSectionButtons() {
-    if (section_buttons_.className === 'display_none');
-    section_buttons_.className = 'display_block';
-    section_home_.className = 'display_none';
-    section_table_.className = 'display_none'
-    section_slider_.className = 'display_none'
-    section_stud_table_.className = 'display_none'
-}
-
-function changeSectionTable() {
-    if (section_table_.className === 'display_none');
-    section_table_.className = 'display_block';
-    section_home_.className = 'display_none'
-    section_buttons_.className = 'display_none'
-    section_slider_.className = 'display_none'
-    section_stud_table_.className = 'display_none'
-}
 
 function changeSectionSlider() {
     if (section_slider_.className === 'display_none');
     section_slider_.className = 'display_block';
-    section_home_.className = 'display_none'
-    section_buttons_.className = 'display_none'
+    section_buttons_.className = 'display_none';
     section_table_.className = 'display_none';
-    section_stud_table_.className = 'display_none'
-}
+    section_stud_table_.className = 'display_none';
+};
+
+function changeSectionButtons() {
+    if (section_buttons_.className === 'display_none');
+    section_buttons_.className = 'display_block';
+    section_table_.className = 'display_none';
+    section_slider_.className = 'display_none';
+    section_stud_table_.className = 'display_none';
+};
+
+function changeSectionTable() {
+    if (section_table_.className === 'display_none');
+    section_table_.className = 'display_block';
+    section_buttons_.className = 'display_none';
+    section_slider_.className = 'display_none';
+    section_stud_table_.className = 'display_none';
+};
+
 
 function changeSectionStudTable() {
     if (section_stud_table_.className === 'display_none');
     section_stud_table_.className = 'display_block';
-    section_home_.className = 'display_none'
-    section_buttons_.className = 'display_none'
+    section_buttons_.className = 'display_none';
     section_table_.className = 'display_none';
     section_slider_.className = 'display_none';
-}
+};
--- layout 10.03.19/assets/slider.js
+++ layout 10.03.19/assets/slider.js
@@ -1,25 +1,29 @@
-function makeSlider() {
-
-    function Slider() {
-        this.init();
-    };
-
-    Slider.prototype.init = function () {
-        this.autoSlider(2000);
-    };
-
-    Slider.prototype.autoSlider = function (timeout) {
-        var left = 0;
-        setInterval(function () {
-            var wrapper = document.querySelector('.slider-wrapper');
-            left = left - 600;
-            if (left < -1200) {
-                left = 0;
-            }
-            wrapper.style.left = left + 'px';
-        }, timeout);
-    }
-    slider = new Slider;
+function Slider(elem) {
+    this.elem = document.querySelector(elem);
+    this.init();
 };
 
-makeSlider();
+Slider.prototype.init = function () {
+    this.wrapper = this.elem.querySelector('.slider-wrapper');
+    this.slides = this.elem.querySelectorAll('.slide');
+    this.slide = this.elem.querySelector('.slide');
+    this.autoSlider(this.wrapper, this.slides, this.slide, 2000);
+};
+
+Slider.prototype.autoSlider = function (wrapper, slides, slide, timeout) {
+    let index = 1;
+    var max = slides.length;
+    var size = slide.clientWidth;
+    let timerId = setInterval(function () {
+        wrapper.style.transition = 'transform .3s ease-in-out';
+        index >= max - 1 ? false : index++;
+        wrapper.style.transform = 'translateX(' + (-index * size) + 'px)';
+        wrapper.addEventListener('transitionend', function () {
+            slides[index].id === 'firstClone' ? index = 1 : index;
+            wrapper.style.transition = 'none';
+            wrapper.style.transform = 'translateX(' + (-index * size) + 'px)';
+        });
+    }, timeout);
+};
+
+slider = new Slider('#main-slider');
